81cf36e59a11c859d768b24d6480165a3d71e7ec
Aditya Pratama <95411404+Aditypraa@users.noreply.github.com>
2024-09-02 19:15:19 +0700

    Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import Filters from "./components/Filters/Filters";
 import Cards from "./components/Cards/Cards";
+import { useEffect, useState } from "react";
 
 function App() {
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  console.log(results);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    (async () => {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
